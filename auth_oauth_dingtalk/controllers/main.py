661f0c830b75ec94298c64ddf1dedc060bee1e2b
林俊 <sum1201@gmail.com>
2018-09-01 19:00:34 +0800

    track bug

--- auth_oauth_dingtalk/controllers/main.py
+++ auth_oauth_dingtalk/controllers/main.py
@@ -97,7 +97,7 @@ class OAuthController(Controller):
         userid = self.get_userid_by_unionid(access_token, userinfo['unionid'])
         try:
             _logger.info("track...........")
-            _logger.info("cre:%s",provider_id,userid)
+            _logger.info("cre:%s" %(provider_id,userid))
             credentials = request.env['res.users'].sudo().auth_oauth_dingtalk(provider_id, userid)
             _logger.info("credentials: %s",credentials)
             url = '/web'
