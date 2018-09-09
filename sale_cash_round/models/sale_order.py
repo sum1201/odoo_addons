a92781d97ae78798b3001cfcf2f51720ab971977
林俊 <sum1201@gmail.com>
2018-09-09 10:02:48 +0800

    update

--- sale_cash_round/models/sale_order.py
+++ sale_cash_round/models/sale_order.py
@@ -14,9 +14,18 @@ class SaleOrder(models.Model):
 
     cash_rounding_id = fields.Many2one('account.cash.rounding', string=u'抹零方式',default=_get_default_cash_rounding_id,
                                        readonly=True, states={'draft': [('readonly', False)]},)
+    auto_cash_round=fields.Boolean(u'自动抹零')
 
-    @api.onchange('order_line','cash_rounding_id')
+    @api.onchange('order_line','cash_rounding_id','auto_cash_round')
     def _onchange_cash_rounding(self):
+        if self.auto_cash_round:
+            self._cash_rounding()
+
+    @api.multi
+    def button_cash_round(self):
+        self._cash_rounding()
+
+    def _cash_rounding(self):
         lines_to_remove = self.order_line.filtered(lambda l: l.is_rounding_line)
         if lines_to_remove:
             self.order_line -= lines_to_remove
@@ -25,11 +34,11 @@ class SaleOrder(models.Model):
             rounding_amount = self.cash_rounding_id.compute_difference(self.currency_id, self.amount_total)
             if not self.currency_id.is_zero(rounding_amount):
                 rounding_line = self.env['sale.order.line'].new({
-                    'product_id':self.cash_rounding_id.product_id.id,
+                    'product_id': self.cash_rounding_id.product_id.id,
                     'name': self.cash_rounding_id.name,
                     'order_id': self.id,
                     'price_unit': rounding_amount,
-                    'product_uom':self.cash_rounding_id.product_id.uom_id.id,
+                    'product_uom': self.cash_rounding_id.product_id.uom_id.id,
                     'product_uom_qty': 1,
                     'is_rounding_line': True,
                     'sequence': 9999  # always last line
@@ -37,7 +46,6 @@ class SaleOrder(models.Model):
 
                 if not rounding_line in self.order_line:
                     self.order_line += rounding_line
-
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
---
+++ web_widget_image_webcam/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Siddharth Bhalgami <siddharth.bhalgami@techreceptives.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Web Widget - Image WebCam",
+    "summary": "Allows to take image with WebCam",
+    "version": "11.0.1.0.0",
+    "category": "web",
+    "website": "https://www.techreceptives.com",
+    "author": "Tech Receptives, "
+              "Odoo Community Association (OCA), "
+              "Kaushal Prajapati",
+              "山西清水欧度信息技术有限公司"
+    "license": "LGPL-3",
+    "data": [
+        "views/assets.xml",
+    ],
+    "depends": [
+        "web",
+    ],
+    "qweb": [
+        "static/src/xml/web_widget_image_webcam.xml",
+    ],
+    "installable": True,
+}
