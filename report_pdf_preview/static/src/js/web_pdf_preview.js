30fea069c20ff1d7ee4ab30ca0e9daeb0a5a4042
山西清水欧度信息技术有限公司 <lin.jun@odooqs.com>
2021-03-26 14:35:09 +0800

    更新到版本14.0 删除旧版本的pdf相关库。 直接采用web模块下的PDF JS， 代码更简洁，只有非常简短的几行代码就搞定

--- report_pdf_preview/static/src/js/web_pdf_preview.js
+++ report_pdf_preview/static/src/js/web_pdf_preview.js
@@ -1,31 +1,25 @@
 odoo.define('report_pdf_preview.report', function (require) {
     'use strict';
 
-    var ActionManager = require('web.ActionManager');
-    var framework = require('web.framework');
-
-    var PreviewDialog = require('report_pdf_preview.PreviewDialog');
+    const ActionManager = require('web.ActionManager');
+    const { qweb} = require('web.core');
+    const Dialog = require('web.Dialog');
 
     ActionManager.include({
         _executeReportAction: function (action, options) {
-            var self = this;
+            const self = this;
             action = _.clone(action);
 
             if (action.report_type === 'qweb-pdf') {
                 return this.call('report', 'checkWkhtmltopdf').then(function (state) {
-                    var active_ids_path = '/' + action.context.active_ids.join(',');
-                    // var url = '/report/pdf/' + action.report_name + active_ids_path;
-                    var url = self._makeReportUrls(action)['pdf'];
-                    var filename = action.report_name;
-                    var title = action.display_name;
-                    var def = $.Deferred()
-                    var dialog = PreviewDialog.createPreviewDialog(self, url, false, "pdf", title);
-                    $.when(dialog, dialog._opened).then(function (dialog) {
-                        var a = 1;
-                        dialog.$modal.find('.preview-download').hide();
+                    const url = `/web/static/lib/pdfjs/web/viewer.html?file=${self._makeReportUrls(action).pdf}`
 
-                    })
-                    framework.unblockUI();
+                    const dialog = new Dialog(this, {
+                        title: action.name,
+                        size: 'large',
+                        $content: $(qweb.render('report_pdf_preview.ReportViewer', {url: url})),
+                    });
+                    dialog.open();
                 });
 
             } else {
