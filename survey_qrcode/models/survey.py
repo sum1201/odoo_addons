033ee267247681022880471b7521863eb342642b
山西清水欧度信息技术有限公司 <lin.jun@odooqs.com>
2021-08-30 09:19:59 +0800

    更改连接

--- survey_qrcode/models/survey.py
+++ survey_qrcode/models/survey.py
@@ -1,17 +1,19 @@
 from odoo import fields, models, api
 import io, base64
+import werkzeug
 
 
 class Survey(models.Model):
     _inherit = 'survey.survey'
 
-    qrcode = fields.Binary('二维码', compute='_compute_session_link')
+    qrcode = fields.Binary('二维码', compute='_compute_qrcode')
 
-    @api.depends('session_code')
-    def _compute_session_link(self):
-        super(Survey, self)._compute_session_link()
-        for survey in self:
+    @api.depends('access_token')
+    def _compute_qrcode(self):
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        for survey_id in self:
             data = io.BytesIO()
             import qrcode
-            qrcode.make(survey.session_link, box_size=4).save(data, optimise=True, format='PNG')
-            survey.qrcode = base64.b64encode(data.getvalue()).decode()
+            survey_start_url = werkzeug.urls.url_join(base_url, survey_id.get_start_url()) if survey_id else False
+            qrcode.make(survey_start_url, box_size=4).save(data, optimise=True, format='PNG')
+            survey_id.qrcode = base64.b64encode(data.getvalue()).decode()
